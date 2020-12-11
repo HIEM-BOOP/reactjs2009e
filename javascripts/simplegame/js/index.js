85aad9bd9731ee7b31212f93718af2b2985df4fc
hiemkbr <hiemkbr@gmail.com>
2020-12-12 00:56:12 +0700

    hiem

--- javascripts/simplegame/js/index.js
+++ javascripts/simplegame/js/index.js
@@ -20,26 +20,35 @@ class Player {
         this.width = width;
         this.heigh = heigh;
         this.step = step;
+        this.gold = 0;
     }
 
     moveDown = () => {
-        this.y += this.step;
+        if (this.y < screenHeigh) {
+            this.y += this.step;
+        }
+        checkWin();
     };
 
     moveRight = () => {
-        this.x += this.step;
+        if (this.x + this.width + this.step < screenHeigh) {
+            this.x += this.step;
+        }
+        checkWin();
     };
 
     moveUp = () => {
         if (this.y >= this.step) {
             this.y -= this.step;
         }
+        checkWin();
     };
 
     moveLeft = () => {
         if (this.x >= this.step) {
             this.x -= this.step;
         }
+        checkWin();
     };
 
     draw = (ctx) => {
@@ -47,7 +56,7 @@ class Player {
     };
 }
 
-var player = new Player(0, 0, 50, 50, 2);
+var player = new Player(0, 0, 50, 50, 10, imgElem);
 // var player2 = new Player(250, 250);
 
 window.onload = () => {
@@ -78,9 +87,75 @@ document.addEventListener("keydown", (event) => {
     }
 });
 
+var trapImg = document.createElement("img")
+trapImg.src = "images/Snap_Trap_icon.png";
+
+var goldImg = document.createElement("img")
+
+goldImg.src = "images/unnamed.png";
+
+var map = [
+    [0, 0, 0, -1, 0, 0, 1, 0, 1, 1, 0],
+    [0, 0, 0, -1, 0, 0, 1, 0, 1, 1, 0],
+    [0, 0, 0, -1, 0, 0, 1, 0, 1, 1, 0],
+    [0, 0, 0, -1, 0, 0, 1, 0, 1, 1, 0],
+    [0, 0, 0, -1, 0, 0, 1, 0, 1, 1, 0],
+    [0, 0, 0, -1, 0, 0, 1, 0, 1, 1, 0],
+    [0, 0, 0, -1, 0, 0, 1, 0, 1, 1, 0],
+    [0, 0, 0, -1, 0, 0, 1, 0, 1, 1, 0],
+    [0, 0, 0, -1, 0, 0, 1, 0, 1, 1, 0],
+    [0, 0, 0, -1, 0, 0, 1, 0, 1, 1, 0],
+
+
+]
+
 // fps = frame per second
+
+
+var numberOfGold = 0;
+map.forEach(cells => {
+    cells.forEach(cell => {
+        if (cell === 1) {
+            numberOfGold++;
+        }
+    })
+})
+
+
+function checkWin() {
+
+    // Check palyer collect gold
+    const playerXIndex = player.x / 50  
+    const playerYIndex = player.y / 50 
+    if(map[playerXIndex][playerYIndex] === 1){
+
+        player.goldCollected(1);
+        map[playerXIndex][playerYIndex] = 0 ;
+
+        }
+
+    if (player.x, player.y)
+        if (player.gold === numberOfGold) {
+            alert("you Win!");
+        }
+}
+
+
+
+
 setInterval(() => {
     clearScreen();
     player.draw(ctx);
     // player2.draw(ctx);
-}, 1);
+    for (var i = 0; i < map.length; i++) {
+        for (var j = 0; j < map[i].length; j++) {
+            if (map[i][j] === -1) {
+                console.log("i , j", i, j);
+                ctx.drawImage(trapImg, j * 50, i * 50, 50, 50)
+            }
+            if (map[i][j] === 1) {
+                ctx.drawImage(goldImg, j * 50, i * 50, 50, 50);
+            }
+        }
+    }
+}, 50);
